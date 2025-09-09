d863674000ef43da1bea8971e7b52fd47c0a86d6
unknown <nabpessoal@gmail.com>
2025-09-09 14:35:23 -0300

    Second Commit

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//2. Criar um projeto com uma rota POST /soma que receba 3 números no body e retorne o resultado da soma dos números recebidos.
+//Verifique antes da soma se os os campos recebidos possuem valores numéricos;
+//Se necessário, informe ao cliente os problemas encontrados;
+
 const express = require("express");
 const app = express();
 const PORT = 8081;
@@ -7,14 +11,19 @@ app.use(express.json()); //Declara que o express vai precisar trabalhar com JSON
 app.post("/mensagem", (req, res) => {
     try {
         const { mensagem: { nome, idade, timeFavorito } } = req.body;
-        console.log(`Olá, ${nome}! Você tem ${idade} anos e torce para o ${timeFavorito}!`); //Retorna ao console as informações obtidas
-        res.status(201).json({ message: `Olá, ${nome}! Você tem ${idade} anos e torce para o ${timeFavorito}!` }); //Retorna as informações obtidas com sucesso (200)
+
+        if (isNaN(idade)) { //I
+            res.status(400).json({ message: `Erro: A idade deve ser um número` });
+        } else {
+
+            console.log(`Olá, ${nome}! Você tem ${idade} anos e torce para o ${timeFavorito}!`); //Retorna ao console as informações obtidas
+            res.status(201).json({ message: `Olá, ${nome}! Você tem ${idade} anos e torce para o ${timeFavorito}!` }); //Retorna as informações obtidas com sucesso (200)
+        }
     } catch (error) { //Parte para dar uma mensagem de erro em caso de erro
         console.error(`Erro: `, error);
         res.status(500).json({ errorMessage: error });
     }
 })
-
 app.listen(PORT, () => {
     console.log(`Servidor rodando em localhost:${PORT}`);
 })
