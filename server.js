6fcd7d33b05a779720c417c175c1ef56c91d8f5d
unknown <nabpessoal@gmail.com>
2025-09-09 14:58:01 -0300

    First Commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//2. Criar um projeto com uma rota POST /soma que receba 3 números no body e retorne o resultado da soma dos números recebidos.
-//Verifique antes da soma se os os campos recebidos possuem valores numéricos;
-//Se necessário, informe ao cliente os problemas encontrados;
+// 3. Criar um projeto com uma rota POST /login que receba um usuário e senha no body e verifique:
+// O usuário e senha infomados estão corretos.
+// Se os valores forem diferentes do esperado, retorne um erro no formato json informado o usuário.
 
 const express = require("express");
 const app = express();
@@ -8,16 +8,15 @@ const PORT = 8081;
 
 app.use(express.json()); //Declara que o express vai precisar trabalhar com JSON.
 
-app.post("/soma", (req, res) => {
+app.post("/login", (req, res) => {
     try {
-        const { soma: { num1, num2, num3 } } = req.body;
+        const { login: { usuario, senha } } = req.body;
 
-        if (isNaN(num1) || isNaN(num2) || isNaN(num3)) { //I
-            res.status(400).json({ message: `Erro: O número deve ser um número` });
+        if (usuario == "nicolas" && senha == 1234) {
+            console.log(`Seja bem vindo(a), ${usuario}! Senha correta`); //Retorna ao console as informações obtidas
+            res.status(201).json({ message: `Seja bem vindo(a), ${usuario}! Senha correta` }); //Retorna as informações obtidas com sucesso (200)
         } else {
-            const soma = num1+num2+num3; // Constante criada para somar todos os números
-            console.log(`${num1}+${num2}+${num3}+=${soma}`); //Retorna ao console as informações obtidas
-            res.status(201).json({ message: `${num1}+${num2}+${num3}+=${soma}` }); //Retorna as informações obtidas com sucesso (200)
+            res.status(403).json({ message: `Erro: Usuário ou senha inválido.` }); //Retorna erro (403)
         }
     } catch (error) { //Parte para dar uma mensagem de erro em caso de erro
         console.error(`Erro: `, error);
